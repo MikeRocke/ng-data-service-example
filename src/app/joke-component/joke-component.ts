2b97ecdd5f274c9d571cf05175c9c21af17f6074
Mike Rocke <mikerocke1989@gmail.com>
2025-10-04 00:23:33 +0100

    using form group to demo validation and pulling the object out

--- src/app/joke-component/joke-component.ts
+++ src/app/joke-component/joke-component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 import { JokeService } from '../joke-service';
 import { AsyncPipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-joke-component',
-  imports: [AsyncPipe, FormsModule],
+  imports: [AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './joke-component.html',
   styleUrl: './joke-component.scss'
 })
 export class JokeComponent {
-  myJoke: string = "";
+
+  jokeForm: FormGroup = new FormGroup({
+    joke: new FormControl('', [Validators.required, Validators.minLength(3)])
+  })
 
   constructor(public jokeService: JokeService) {
 
   }
 
   updateJoke() {
-    console.log(this.myJoke);
-    this.jokeService.setJoke(this.myJoke);
+    let jokeData : JokeData = <JokeData> this.jokeForm.getRawValue();
+    this.jokeService.setJoke(jokeData.joke);
   }
+}
+
+export interface JokeData {
+  joke: string
 }
